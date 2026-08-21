72b465799ea3a58e0036a533036d4d6bc9a23200
hafsanuktar011-crypto <hafsamuktar011@gmail.com>
2026-08-21 23:36:00 +0300

    feat:add header components with logo

---
+++ Netflex/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./Header.module.css";
+import logo from "../../assets/images/logo (1).png";
+
+function Header() {
+  return (
+    <header>
+      {/* logo */}
+      <img src={logo} alt="Logo" />
+
+      {/* navigation links */}
+
+      {/* right side section */}
+    </header>
+  );
+}
+
+export default Header;
