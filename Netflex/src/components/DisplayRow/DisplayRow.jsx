236eb8286e877f542a72d4972279b2e4e3b27dd6
hafsanuktar011-crypto <hafsamuktar011@gmail.com>
2026-08-29 11:32:56 +0300

    feat: integrate movie API requests in display row

--- Netflex/src/components/DisplayRow/DisplayRow.jsx
+++ Netflex/src/components/DisplayRow/DisplayRow.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SlideShow from '../SlideShow/SlideShow'
 import styles from './DisplayRow.module.css'
 import { movies } from './../../Data/Data';
+import { movieInstance } from '../../utility/MoviesInstance';
+import requests from '../../utility/requestUrl';
 
 function DisplayRow() {
+
+const [movies,setMovies]=useState({
+    trending:[],
+    netflixoriginals:[],
+    topRated:[],
+    action:[],
+    comedy:[],
+    horror:[],
+    romance:[],
+    documentaries:[]
+})
+
+const fetchMovies=async()=>{
+    try{
+ const[trending,
+    netflixoriginals,
+    topRated,
+    action,
+    comedy,
+    horror,
+    romance,
+    documentaries]=await Promise.all(
+        movieInstance.get(requests.fetchTrending),
+        movieInstance.get(requests.fetchNetflixOriginals),
+        movieInstance.get(requests.fetchTopRatedMovies),
+        movieInstance.get(requests.fetchActionMovies),
+        movieInstance.get(requests.fetchComedyMovies),
+        movieInstance.get(requests.fetchDocumentaries),
+        movieInstance.get(requests.fetchRomanceMovies),
+        movieInstance.get(requests.fetchHorrorMovies)
+    )
+    }
+    catch(error){
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   return (
     <div className={styles.mainRapper}>
 <SlideShow title="Movie Suggestions" movies={movies} />
